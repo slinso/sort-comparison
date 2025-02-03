77110a57447ef2351d4a7979940eaa073e0fef38
slinso <sven@lindow-software.de>
2025-02-03 12:23:23 +0100

    refactor: simplify InsertionSort implementation by delegating to insertionSort function

--- insertionsort.go
+++ insertionsort.go
@@ -20,21 +20,5 @@ Implementation Notes:
   - Practical for small arrays (n < 50)
 */
 func InsertionSort(arr []int) {
-	n := len(arr)
-	if n <= 1 {
-		return
-	}
-
-	for i := 1; i < n; i++ {
-		key := arr[i]
-		j := i - 1
-
-		// Move elements that are greater than key
-		// to one position ahead of their current position
-		for j >= 0 && arr[j] > key {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
+	insertionSort(arr)
 }
